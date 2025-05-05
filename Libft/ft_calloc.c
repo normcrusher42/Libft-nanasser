5b7da1bf5b291b3fb64307e37b460dd456519f66
normcrusher42 <nanasser@student.42abudhabi.ae>
2025-05-05 19:19:19 +0400

    Update ft_calloc.c
    useless i variable for some reason

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -14,17 +14,13 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	size_t	i;
 	char	*ptr;
 
 	if (count > SIZE_MAX / size)
 		return (NULL);
-	i = 0;
 	ptr = malloc(count * size);
 	if (!ptr)
-	{
 		return (NULL);
-	}
 	ft_bzero(ptr, count * size);
 	return (ptr);
 }
